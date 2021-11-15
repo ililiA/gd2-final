15cd09a5370d361de62dde2521340ba634c3dd34
ililiA <liliburger13@gmail.com>
2021-11-14 21:08:19 -0600

    Week 13

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Follower : MonoBehaviour
+{
+    public Animator anim;
+    public Transform player;
+
+    private NavMeshAgent agent;
+
+    private bool waiting = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting Start");
+        agent = this.GetComponent<NavMeshAgent>();
+
+        // keep it form stopping at each patrol point
+        // agent.autoBraking = false;
+    }
+
+/*
+    IEnumerator GoToNextPoint()
+    {
+        Debug.Log("Starting GoToNextPoint()");
+        // if no points exist
+        if(points.Length == 0)
+        {
+            yield return new WaitForEndOfFrame();;     //exit this method()
+        }
+
+        // wait for 2 seconds
+        Debug.Log("Starting ToWait");
+        waiting = true;
+        agent.destination = this.transform.position;
+        yield return new WaitForSeconds(2);
+        waiting = false;
+        
+        // set the agent to go to the currently selected destination
+        agent.destination = points[destPoint].position;
+
+        //choose the next point in the array as the destination,
+        // cyclying to the start if necessary
+        destPoint = (destPoint + 1) % points.Length;
+    }
+    */
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+        // when the ai gets close to a destination
+        // go to the next point
+        // ! is the NOT operator
+        if(Vector3.Distance(this.transform.position, player.position) > 2)
+        {
+            Debug.Log("Not Following Player");
+            if(!agent.pathPending && agent.remainingDistance < 0.5f && !waiting)
+            {
+              agent.destination = player.position;
+            }
+        }
+        else
+        {
+            Debug.Log("Following Player");
+            agent.destination = this.transform.position;
+        }
+    }
+
+}
